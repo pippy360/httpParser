8b2871d8bef03750387aac078f52144452f65303
Tom <tomnomnom1@gmail.com>
2016-05-23 20:30:06 +0100

    updated the unit test for the header/status line parser

--- makefile
+++ makefile
@@ -2,7 +2,7 @@
 all: payloadTest.out
 
 payloadTest.out: test/httpParserPayload_unitTest.cpp c/httpParserPayload.cpp
-	g++ -Wno-write-strings -std=c++11 -g -O0 ./lib/libgtest_main.a ./lib/libgtest.a test/httpParserPayload_unitTest.cpp -lpthread -o payloadTest.out
+	g++ -Wno-write-strings -std=c++11 -g -O0 ./lib/libgtest_main.a ./lib/libgtest.a test/httpParserPayload_unitTest.cpp test/httpParserHeader_unitTest.cpp  -lpthread -o allTests.out
 
 googleTest.out: 
 	g++ -Wno-write-strings -std=c++11 ./test/sample1.cc ./test/sample1_unittest.cc ./lib/libgtest.a ./lib/libgtest_main.a -lpthread -o ./test.out
---
+++ test/httpParserHeader_unitTest.cpp
@@ -0,0 +1,40 @@
+#include "../include/gtest/gtest.h"
+#include "../c/httpParserHeader.cpp"
+
+
+TEST(getNextRequestStatusLineStateAndByteType, simple_test_case){
+	const char *requestStatusLine = "GET / HTTP/1.1\r\n";
+	const char *currPos = requestStatusLine;
+
+	int validTypes[] = {
+			HEADER_REQUEST_METHOD, 				3,
+			HEADER_REQUEST_SPACE_AFTER_METHOD, 	1,
+			HEADER_REQUEST_PATH, 				1,
+			HEADER_SPACE_AFTER_PATH,			1,
+			HEADER_REQUEST_HTTP_VERSION_H, 		1,
+			HEADER_REQUEST_HTTP_VERSION_T,		1,
+			HEADER_REQUEST_HTTP_VERSION_T_2,	1,
+			HEADER_REQUEST_HTTP_VERSION_P,		1,
+			HEADER_REQUEST_HTTP_VERSION_SLASH,	1,
+			HEADER_REQUEST_HTTP_VERSION_FIRST_NUMBER,	1,
+			HEADER_REQUEST_HTTP_VERSION_DOT_AFTER_FIRST_NUMBER,	1,
+			HEADER_REQUEST_HTTP_VERSION_SECOND_NUMBER,	1,
+			HEADER_REQUEST_NEW_LINE_R,			1,
+			HEADER_REQUEST_FINISHED_PARSING,	1,
+	};
+
+	ReturnValueOfGetNextRequestStatusLineStateAndByteType ret;
+	StatusLineRequestHeaderParserStateEnum nextStateVal = HEADER_REQUEST_START;
+
+	for(int i = 0; i < 12; i++){
+		for(int j=0; j < validTypes[i*2 + 1]; j++){
+			ret = getNextRequestStatusLineStateAndByteType(nextStateVal, *currPos);
+			nextStateVal = ret.statusLineParserStateEnum;
+
+			EXPECT_EQ(validTypes[i*2]	, ret.statusLineParserStateEnum);
+			EXPECT_EQ(false				, ret.isError);
+
+			currPos++;
+		}
+	}
+}
