8194463a914ad0b3f9ea187140f3c89812c2f06f
Tom <tomnomnom1@gmail.com>
2016-05-28 00:22:36 +0100

    fix for the "add to buffer" function

--- c/httpParserCommonUtils.c
+++ c/httpParserCommonUtils.c
@@ -6,7 +6,7 @@
 //returns 0 if success, non-0 otherwise
 int _addToHttpStateBufferWithErrorChecking(char *buffer,
 		int *currentLengthOfBuffer, const int maxSize, const char nextByte) {
-	if (*currentLengthOfBuffer + 2 < maxSize) //+2 for the new char and '\0'
+	if (*currentLengthOfBuffer + 2 <= maxSize) //+2 for the new char and '\0'
 			{
 		//strcpy_s(currState->chunkSizeStrBuffer, &nextByte);
 		strcat(buffer, &nextByte);
